0f1f00881c35019649facf70063ba28917f8d261
Pushpender <56433698+Technowlogy-Pushpender@users.noreply.github.com>
2019-12-03 12:07:15 +0530

    Update installer_linux.sh

--- installer_linux.sh
+++ installer_linux.sh
@@ -8,10 +8,9 @@ sudo apt-get update
 sudo apt-get install -y wine32 python-pip pyinstaller
 wget https://www.python.org/ftp/python/3.7.4/python-3.7.4.exe
 wine msiexec /i python-3.7.4.exe
-sudo wine ~/.wine/drive_c/Python37/python.exe -m pip install pyinstaller 
-sudo pip install pyfiglet
-sudo pip install pynput
+sudo wine ~/.wine/drive_c/Python37/python.exe -m pip install pyinstaller mss==4.0.3 essential_generators==0.9.2 six==1.12.0 pywin32 python-xlib==0.25
 sudo pip install mss==4.0.3
 sudo pip install essential_generators==0.9.2
 sudo pip install six==1.12.0 
 sudo pip install python-xlib==0.25
+sudo pip install pywin32
