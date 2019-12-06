0c9be9b1d4e18f9735e0f038d9e7e17567891a0a
Pushpender <56433698+Technowlogy-Pushpender@users.noreply.github.com>
2019-12-06 19:04:10 +0530

    Add files via upload

--- installer_linux.sh
+++ installer_linux.sh
@@ -15,7 +15,7 @@ wine msiexec /i python-3.7.4.exe
 wine pywin32-227.win32-py3.7.exe
 
 #Installing Dependencies [Python Modules] in Wine
-sudo wine ~/.wine/drive_c/Python37/python.exe -m pip install pyinstaller mss==4.0.3 essential_generators==0.9.2 six==1.12.0 python-xlib==0.25 win32gui pywin32
+sudo wine ~/.wine/drive_c/Python37-32/python.exe -m pip install pyinstaller mss==4.0.3 essential_generators==0.9.2 six==1.12.0 python-xlib==0.25 win32gui pywin32
 
 #Installing Dependencies in Linux 
 sudo python3 -m pip install mss==4.0.3
